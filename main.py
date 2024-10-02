7021170e7bbb8547bc5865b14694d8d81dcb16a3
ulrich-marcelo <mulrich@estudiantes.unsam.edu.ar>
2024-10-02 18:20:23 -0300

    Testeos usando sqlite3

--- main.py
+++ main.py
@@ -1,29 +1,59 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource
+import sqlite3
 
 app = Flask(__name__)
 api = Api(app)
 
-task_parser = reqparse.RequestParser()
-task_parser.add_argument("task_id", type=int,
-                         help="ID number for task", 
-                         required=True)  # No sabemos como usarlo
 todo = dict()
+
+connect = sqlite3.connect('database.db')
+connect.execute("CREATE TABLE IF NOT EXISTS TODOLIST (id INT NOT NULL PRIMARY \
+                KEY, name TEXT, realizado BOOLEAN)")
 
 
 class ToDoList(Resource):
     def get(self, task_id: str):
-        return {task_id: todo[task_id]}
+        with sqlite3.connect("database.db") as users:
+            cursor = users.cursor()
+            cursor.execute("SELECT * FROM TODOLIST WHERE id = ?", (task_id))
+            response = cursor.fetchall()
+
+        return response
 
     def put(self, task_id: int):
-        args = task_parser.parse_args()
-        task_id = args["task_id"]
-        todo[task_id] = request.form[task_id]
-        return {task_id: todo[task_id]}
+        data = request.form
+        id = task_id
+        name = data["name"]
+        real = data["realizado"] == "True"
+
+        response = {"id": id,
+                    "name": name,
+                    "realizado": real}
+
+        with sqlite3.connect("database.db") as users:
+            cursor = users.cursor()
+            try:
+                cursor.execute("INSERT INTO TODOLIST (id,name,realizado) \
+                               VALUES (?,?,?)", (id, name, real))
+            except sqlite3.IntegrityError:
+                cursor.execute("UPDATE TODOLIST SET name = ?, realizado = ? \
+                               WHERE id = ?", (name, real, id))
+            finally:
+                users.commit()
+
+        return response
+
+    def delete(self, task_id: int):
+        with sqlite3.connect("database.db") as users:
+            cursor = users.cursor()
+            cursor.execute("DELETE FROM TODOLIST WHERE id = ?", (task_id))
+            users.commit()
+        return {"message": "Todo ok"}
 
 
-api.add_resource(ToDoList, "/<string:task_id>")
+api.add_resource(ToDoList, "/task<task_id>")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- test.py
+++ test.py
@@ -1,19 +1,36 @@
-from requests import put, get
+from requests import put, get, delete
 
 task_local = {f"task{i}": f"BBB{i}" for i in range(3)}
 # print("Registro actual")
 # print(get("http://127.0.0.1:5000/task1").json())
 
+print("Probando POST")
 for key, value in task_local.items():
     url = f"http://127.0.0.1:5000/{key}"
-    response = put(url, data={"task_id": value})
+    response = put(url, data={"name": value, "realizado": True})
     print(response.json())
     print(response.status_code)
 
+print("Probando GET")
 for key in task_local.keys():
     url = f"http://127.0.0.1:5000/{key}"
     response = get(url)
+    print(response)
     print(response.json())
     print(response.status_code)
 
-print(get("http://127.0.0.1:5000/4"))
+print("Probando DELETE")
+
+url1 = "http://127.0.0.1:5000/task2"
+url2 = "http://127.0.0.1:5000/task3"
+
+res1 = delete(url1)
+print("Eliminando task2")
+print(res1)
+print(res1.json())
+res2 = delete(url2)
+print("Elimnando task3 inexistente, no rompe")
+print(res2)
+print(res2.json())
+print("Chequeo que task2 este eliminado")
+print(get(url1).json())
