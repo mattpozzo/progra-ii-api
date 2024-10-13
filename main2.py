52f5f2871afc29673a8bb3440dec0875416fdb1b
bautistaturri <turribautista551@gmail.com>
2024-10-13 11:59:39 -0300

    feat[PROGRA2]: agregado de resource para buen funcionamiento del url

--- main2.py
+++ main2.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from apis import api
+from apis.login import Login
 
 app = Flask(__name__)
 api.init_app(app)
 
+api.add_resource(Login, '/login/<int:id>')
+
 app.run(debug=True)
+
