f49974393cee3ed063301e2cb2f25180f005cd07
ulrich-marcelo <mulrich@estudiantes.unsam.edu.ar>
2024-11-12 17:38:15 -0300

    Organización de codigo y tests listos

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Api
+from .config import Config
+from .models import db
+from .resources.user import user_ns
+from .resources.gym import gym_ns
+from .resources.usertype import user_type_ns
+from .resources.userTypeGym import user_type_gym_ns
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+
+    api = Api(app, title="My Flask App API", version="1.0", description="A sample API")
+    api.add_namespace(user_ns)
+    api.add_namespace(gym_ns)
+    api.add_namespace(user_type_ns)
+    api.add_namespace(user_type_gym_ns)
+
+
+    with app.app_context():
+        db.create_all()
+
+    return app
--- test.py
+++ test.py
@@ -1,23 +1,77 @@
 from requests import put, get, delete, post
 
+headersJson = {'Content-Type': 'application/json'}
+
 #probando que el token es necesario
 print('Probando que el token es necesario')
 get_users_test1 = get('http://127.0.0.1:5000/users/')
 print(get_users_test1.json())
 
 registro = post('http://127.0.0.1:5000/users/register',
-                headers={'Content-Type': 'application/json'},
+                headers=headersJson,
                 json={"first_name": "Marcelo", 
                       "last_name": "Ulrich", 
                       "email": "mulrich@estudiantes.unsam.edu.ar", 
                       "password": "pass123", 
                       "certified": False})
 
-print('Probando registro...')
+print('\n\nProbando registro...')
 if registro.status_code == 201:
     print('Registro exitoso')
 else:
     print('ERROR en registro: '+registro.json()['message'])
 
+print('\n\nPrueba error login')
+login_failed = post('http://127.0.0.1:5000/users/login',headers=headersJson,json={"email": "laura.martinez@example.com", "password": "securepassword123"})
+print(login_failed.json())
+
+print('\n\nPrueba login exitoso')
+login_success = post('http://127.0.0.1:5000/users/login',headers=headersJson,json={"email": "mulrich@estudiantes.unsam.edu.ar", "password": "pass123"})
+print(login_success.json())
+token = login_success.json()['token']
+
+headersJson['Authorization'] = 'Bearer '+token
+print('Ahora que tenemos token, pruebo acceder a todos los usuarios')
+users = get('http://127.0.0.1:5000/users/',headers=headersJson)
+print(users.json())
+
+print('\n\nPrueba Gimnasios')
+gyms = get('http://127.0.0.1:5000/gyms/',headers=headersJson)
+print(gyms.json())
+
+print('\n\nAñado Gym')
+new_gym = post('http://127.0.0.1:5000/gyms/add',headers=headersJson,json={'name':'HerculesGym','location':'Villa del Parque'})
+print(new_gym.json())
+
+print('\n\nPrueba Gimnasios Otra Vez')
+gyms = get('http://127.0.0.1:5000/gyms/',headers=headersJson)
+print(gyms.json())
 
 
+print('\n\nPrueba UserType')
+gyms = get('http://127.0.0.1:5000/user_types/',headers=headersJson)
+print(gyms.json())
+
+print('\n\nAñado UserType')
+new_gym = post('http://127.0.0.1:5000/user_types/add',headers=headersJson,json={'name':'Premium'})
+print(new_gym.json())
+
+print('\n\nPrueba UserType Otra Vez')
+gyms = get('http://127.0.0.1:5000/user_types/',headers=headersJson)
+print(gyms.json())
+
+
+print('\n\nPrueba UserTypeGym')
+gyms = get('http://127.0.0.1:5000/user_type_gyms/',headers=headersJson)
+print(gyms.json())
+
+print('\n\nAñado UserType')
+new_gym = post('http://127.0.0.1:5000/user_type_gyms/assign',headers=headersJson,json={
+    'user_id':'2',
+    'gym_id':'1',
+    'user_type_id':'1'})
+print(new_gym.json())
+
+print('\n\nPrueba UserType Otra Vez')
+gyms = get('http://127.0.0.1:5000/user_type_gyms/',headers=headersJson)
+print(gyms.json())
