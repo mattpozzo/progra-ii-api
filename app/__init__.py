1d012b3a561c8dc48fead9dc55c4b42afb4ce3a6
bautistaturri <turribautista551@gmail.com>
2024-11-21 08:35:33 -0300

    agregado de recursos de ingredients

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from .resources.user import user_ns
 from .resources.gym import gym_ns
 from .resources.usertype import user_type_ns
 from .resources.userTypeGym import user_type_gym_ns
-
+from .resources.ingredient import ingredients_ns
 
 def create_app():
     app = Flask(__name__)
@@ -19,7 +19,7 @@ def create_app():
     api.add_namespace(gym_ns)
     api.add_namespace(user_type_ns)
     api.add_namespace(user_type_gym_ns)
-
+    api.add_namespace(ingredients_ns)
 
     with app.app_context():
         db.create_all()
