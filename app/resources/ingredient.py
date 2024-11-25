6962bd02814e8c3a3fd16a2e9d8ef69a9d86a74f
bautistaturri <turribautista551@gmail.com>
2024-11-25 18:19:23 -0300

    se comento post en el modelo ingredient

--- app/resources/ingredient.py
+++ app/resources/ingredient.py
@@ -11,24 +11,24 @@ ingredients_ns = Namespace('ingredients', description='Operaciones relacionadas 
 # Crear el recurso relacionado con ingredientes
 @ingredients_ns.route('/')
 class IngredientResource(Resource):
-    def post(self):
-        '''
-        Crear un ingrediente
-        curl -X POST http://localhost:5000/ingredients/ \
-        -H "Content-Type: application/json" \
-        -d '{"name": "Tomate"}'
-        '''
-        data = request.get_json()
-        name = data.get('name')
+    #def post(self):
+    #    '''
+    #    Crear un ingrediente
+    #    curl -X POST http://localhost:5000/ingredients/ \
+    #    -H "Content-Type: application/json" \
+    #    -d '{"name": "Tomate"}'
+    #    '''
+    #    data = request.get_json()
+    #    name = data.get('name')
+#
+    #    if not name:
+     #       return {'message': 'Name is required'}, 400
 
-        if not name:
-            return {'message': 'Name is required'}, 400
+    #    ingredient = Ingredient(name=name)
+    #    db.session.add(ingredient)
+    #    db.session.commit()
 
-        ingredient = Ingredient(name=name)
-        db.session.add(ingredient)
-        db.session.commit()
-
-        return {'id': ingredient.id, 'name': ingredient.name}, 201
+   #     return {'id': ingredient.id, 'name': ingredient.name}, 201
 
     def get(self):
         '''
