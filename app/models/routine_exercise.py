fd5c25a349664f2fc5e6f2b152a3ac90a097e620
ulrich-marcelo <mulrich@estudiantes.unsam.edu.ar>
2024-11-25 19:32:23 -0300

    listos mis models

--- app/models/routine_exercise.py
+++ app/models/routine_exercise.py
@@ -1,19 +1,37 @@
 from app.models import db
 from app.models.audit.base_audit import BaseAudit
 
+
 class RoutineExercise(db.Model, BaseAudit):
     __tablename__ = 'routine_exercise'
-    id = db.Column(db.Integer, primary_key = True)
-    sets = db.Column(db.Integer, nullable = False)
-    reps = db.Column(db.Integer, nullable = False)
-    weight = db.Column(db.Integer, nullable = False)
-    exercise_id = db.Column(db.Integer, db.ForeignKey('exercise.id'), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    sets = db.Column(db.Integer, nullable=False)
+    reps = db.Column(db.Integer, nullable=False)
+    weight = db.Column(db.Integer, nullable=False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercise.id'),
+                            nullable=False)
     notes = db.Column(db.String(512))
     session_id = db.Column(db.Integer, db.ForeignKey('session.id'))
-    routine_id = db.Column(db.Integer, db.ForeignKey('routine.id'), nullable = False)
+    routine_id = db.Column(db.Integer, db.ForeignKey('routine.id'),
+                           nullable=False)
 
-    _exercise = db.relationship('Exercise', backref= db.backref('routine_exercises', lazy=True))
-    _session = db.relationship('Session', backref= db.backref('routine_exercise', lazy=True))
-    _routine = db.relationship('Routine', backref= db.backref('routine_exercises', lazy=True))
+    _exercise = db.relationship('Exercise',
+                                backref=db.backref('routine_exercises',
+                                                   lazy=True))
+    _session = db.relationship('Session',
+                               backref=db.backref('routine_exercise',
+                                                  lazy=True))
+    _routine = db.relationship('Routine',
+                               backref=db.backref('routine_exercises',
+                                                  lazy=True))
 
-    #que pasaría con el serialize si no hay session?
+    def serialize(self):
+        return super().serialize() | {
+            "id": self.id,
+            'sets': self.sets,
+            'reps': self.reps,
+            'weight': self.weight,
+            'exercise': self._exercise.serialize(),
+            'session': self._session.serialize() if self._session else None,
+            'routine': self._routine.serialize()
+        }
---
+++ app/models/routine_schedule.py
@@ -0,0 +1,30 @@
+from app.models import db
+from app.models.audit.base_audit import BaseAudit
+from sqlalchemy import Time
+
+
+class RoutineSchedule(db.Model, BaseAudit):
+    __tablename__ = 'routine_schedule'
+    id = db.Column(db.Integer, primary_key=True)
+
+    weekday = db.Column(db.Integer, nullable=False)
+    hour = db.Column(Time, nullable=False)
+
+    training_plan_id = db.Column(db.Integer, db.ForeignKey('training_plan.id'),
+                                 nullable=False)
+    routine_id = db.Column(db.Integer, db.ForeignKey('routine.id'),
+                           nullable=False)
+
+    _training_plan = db.relationship('TrainigPlan', 
+                                     backref=db.backref('routine_schedules',
+                                                        lazy=True))
+    _routine = db.relationship('Routine')
+
+    def serialize(self):
+        return super().serialize() | {
+            "id": self.id,
+            'weekday': self.weekday,
+            'hour': self.hour,
+            'training_plan': self._training_plan.serialize(),
+            'routine': self._routine.serialize() if self._gym else None
+        }
