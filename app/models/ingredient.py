913ca53258c944447829575032613a0f499bda0f
bautistaturri <turribautista551@gmail.com>
2024-11-26 09:15:54 -0300

    agregado de condiciones para no tener duplicados en ingredients

--- app/models/ingredient.py
+++ app/models/ingredient.py
@@ -3,4 +3,4 @@ from . import db
 class Ingredient(db.Model):
     __tablename__ = 'ingredient'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False, unique=True)  # unique=True para evitar duplicados
