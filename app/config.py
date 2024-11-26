7016ea75d825aea0d7e73cb33ac25c83ce2869b7
bautistaturri <turribautista551@gmail.com>
2024-11-26 15:39:44 -0300

    cambios

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI',"postgresql://postgres:hola1234T@localhost/PostgresIn10")
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')  # Asegúrate que sea una cadena
+    SECRET_KEY = os.getenv('DB_SECRET')
     
