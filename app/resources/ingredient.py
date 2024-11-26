6d731a235d4d917ab1d4099521d97735043a9c4e
bautistaturri <turribautista551@gmail.com>
2024-11-25 21:03:20 -0300

    agregado de post en ingredient

--- app/resources/ingredient.py
+++ app/resources/ingredient.py
@@ -5,30 +5,30 @@ from app.models.ingredient import Ingredient
 
 
 
-# Crear el namespace
+# namespace
 ingredients_ns = Namespace('ingredients', description='Operaciones relacionadas con los ingredientes')
 
-# Crear el recurso relacionado con ingredientes
+# recurso relacionado con ingredientes (junto a sus metodos post y get)
 @ingredients_ns.route('/')
 class IngredientResource(Resource):
-    #def post(self):
-    #    '''
-    #    Crear un ingrediente
-    #    curl -X POST http://localhost:5000/ingredients/ \
-    #    -H "Content-Type: application/json" \
-    #    -d '{"name": "Tomate"}'
-    #    '''
-    #    data = request.get_json()
-    #    name = data.get('name')
-#
-    #    if not name:
-     #       return {'message': 'Name is required'}, 400
+    def post(self):
+        '''
+        Crear un ingrediente
+        curl -X POST http://localhost:5000/ingredients/ \
+        -H "Content-Type: application/json" \
+        -d '{"name": "Tomate"}'
+       '''
+        data = request.get_json()
+        name = data.get('name')
 
-    #    ingredient = Ingredient(name=name)
-    #    db.session.add(ingredient)
-    #    db.session.commit()
+        if not name:
+            return {'message': 'Name is required'}, 400
 
-   #     return {'id': ingredient.id, 'name': ingredient.name}, 201
+        ingredient = Ingredient(name=name)
+        db.session.add(ingredient)
+        db.session.commit()
+
+        return {'id': ingredient.id, 'name': ingredient.name}, 201
 
     def get(self):
         '''
@@ -38,9 +38,9 @@ class IngredientResource(Resource):
         ingredients = Ingredient.query.all()
         return [{'id': ingredient.id, 'name': ingredient.name} for ingredient in ingredients], 200
 
-@ingredients_ns.route('/<int:id>')  # Define la ruta /ingredients/<id>
+@ingredients_ns.route('/<int:id>')  # ruta /ingredients/<id>
 class IngredientByIdResource(Resource):
-    # GET para obtener un ingrediente específico por ID
+    # GET para obtener un ingrediente especifico por ID
     '''
     curl -X GET http://localhost:5000/ingredients/1
     '''
