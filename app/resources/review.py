046e03965401aae23c7faa9e61e3c2fedf584bfc
bautistaturri <turribautista551@gmail.com>
2024-11-28 00:28:23 -0300

    agregado de review en resource

--- app/resources/review.py
+++ app/resources/review.py
@@ -75,3 +75,135 @@ class RecipeReviewsResource(Resource):
         """
         reviews = Review.query.filter_by(recipe_id=recipe_id).all()
         return reviews, 200
+
+#POST PARA ASIGNAR PUNTUACION A UN GIMNASIO
+@review_ns.route('/gym/<int:id>')
+class ReviewGymResource(Resource):
+    @review_ns.doc('create_review_for_gym')
+    @review_ns.expect(review_model)  
+    def post(self, id):
+        """
+        Crear una nueva reseña para un gimnasio.
+        curl -X POST http://localhost:5000/reviews/gym/<id> \
+        -H "Content-Type: application/json" \
+        -d '{"score": 5, "comment": "Excelente gimnasio!"}'
+        """
+        data = request.get_json()
+        score = data.get('score')
+        comment = data.get('comment')
+        
+        
+        if not score or not comment:
+            return {'message': 'Score and comment are required'}, 400
+        
+        
+        gym = Gym.query.get(id)
+        if not gym:
+            return {'message': 'Gym not found'}, 404
+        
+        
+        review = Review(score=score, comment=comment, gym_id=id)
+        
+        
+        db.session.add(review)
+        db.session.commit()
+
+        return review.serialize(), 201
+    
+
+#GET PARA RECETAS POR GIMNASIO Y USUARIO
+@review_ns.route('/')
+class ReviewListResource(Resource):
+    @review_ns.doc('get_reviews')
+    def get(self):
+        """
+        Obtener reseñas filtradas por receta, gimnasio y usuario.
+        curl -X GET http://localhost:5000/reviews?recipe=<id>&gym=<id>&user=<id>
+        """
+        recipe_id = request.args.get('recipe')
+        gym_id = request.args.get('gym')
+        user_id = request.args.get('user')
+
+        
+        query = Review.query
+
+        if recipe_id:
+            query = query.filter_by(recipe_id=recipe_id)
+        if gym_id:
+            query = query.filter_by(gym_id=gym_id)
+        if user_id:
+            query = query.filter_by(user_id=user_id)
+
+        reviews = query.all()
+
+        return [review.serialize() for review in reviews], 200
+
+
+
+
+@review_ns.route('/<int:id>')
+class ReviewResource(Resource):
+    @review_ns.doc('update_review')
+    @review_ns.expect(review_model)  # Asegúrate de que review_model esté definido correctamente
+    def patch(self, id):
+        """
+        Actualizar una reseña por su ID.
+        curl -X PATCH http://localhost:5000/reviews/<id> \
+        -H "Content-Type: application/json" \
+        -d '{"score": 4, "comment": "Buena receta, pero mejorable."}'
+        """
+        data = request.get_json()
+        score = data.get('score')
+        comment = data.get('comment')
+
+        review = Review.query.get(id)
+        if not review:
+            return {'message': 'Review not found'}, 404
+
+        # Actualizar los campos
+        if score is not None:
+            review.score = score
+        if comment:
+            review.comment = comment
+        
+        db.session.commit()
+
+        return review.serialize(), 200
+
+
+
+
+@review_ns.route('/')
+class ReviewListResource(Resource):
+    @review_ns.doc('get_all_reviews')
+    def get(self):
+        """
+        Obtener todas las reseñas.
+        curl -X GET http://localhost:5000/review/
+        """
+        reviews = Review.query.all()
+
+        if not reviews:
+            return {'message': 'No reviews found'}, 404
+
+        return [review.serialize() for review in reviews], 200
+
+
+
+
+@review_ns.route('/<int:id>')
+class ReviewResource(Resource):
+    @review_ns.doc('delete_review')
+    def delete(self, id):
+        """
+        Eliminar una reseña por su ID.
+        curl -X DELETE http://localhost:5000/review/<id>
+        """
+        review = Review.query.get(id)
+        if not review:
+            return {'message': 'Review not found'}, 404
+
+        db.session.delete(review)
+        db.session.commit()
+
+        return {'message': 'Review deleted successfully'}, 200
